499f4912766b24bf3a732582a6ea13cc106407b0
erpsupporttecfuge <erpsupport@tecfuge.com>
2023-02-09 23:23:21 +0530

    “chaged_store”

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,23 +1,27 @@
-import { createStore } from 'redux'
- const reducerFn = (state={counter:0},action) =>{
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+ 
+const counterSlice = createSlice({
+name : 'counter',
+initialState :{ counter: 0},
+reducers:{
+    increment(state,action){
+       state.counter= state.counter +1 
+    },
+    decrement(state,action){
+        state.counter= state.counter -1 
 
-    // sychronous Function
-    // we should not mutate the original state
-    if (action.type==='INC'){
 
-        return {counter: state.counter + 1}
+    },
+    addBy(state,action){
+
+        state.counter= state.counter +1 
+
     }
+}
 
-    if (action.type==='DIC'){
 
-        return {counter: state.counter - 1}
-    }
-    if (action.type==='ADD10'){
-
-        return {counter: state.counter + 10}
-    }
-    return state
-
- }
- const store = createStore(reducerFn);
- export default store
+})
+export const actions = counterSlice.actions;
+const store = configureStore({
+    reducer : counterSlice.reducer
+}) 
